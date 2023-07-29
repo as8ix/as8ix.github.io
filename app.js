dfc205f34cbab5a7e179d77aa981d8936a1e8536
xoiu77 <abdallaabdiaziz67@gmail.com>
2023-07-29 06:59:50 +0300

    hh

--- app.js
+++ app.js
@@ -69,11 +69,24 @@ smallImg[0].onclick = function () {
     MainImg.src = smallImg[0].src;
 }
 smallImg[1].onclick = function () {
-    MainImg.src = smallImg[2].src;
+    MainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function () {
+    MainImg.src = smallImg[3].src;
+}
+smallImg[3].onclick = function () {
+    MainImg.src = smallImg[4].src;
+}
+
+smallImg[3].onclick = function () {
+    MainImg.src = smallImg[3].src;
 }
 smallImg[2].onclick = function () {
     MainImg.src = smallImg[2].src;
 }
-smallImg[3].onclick = function () {
-    MainImg.src = smallImg[3].src;
+smallImg[1].onclick = function () {
+    MainImg.src = smallImg[1].src;
+}
+smallImg[0].onclick = function () {
+    MainImg.src = smallImg[0].src;
 }
